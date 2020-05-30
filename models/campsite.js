6ad01971bbe8a16be44a24e16405269e82056de0
Jason <jasonfyfe.dev@gmail.com>
2020-05-30 02:36:22 +0100

    Geocoding and Google Maps
    Finalised implementation of Google Maps.
    This commit includes:
    - Addition of Node Geocoder
    - Revised implementation of Google Maps
    - New/Edit Campsite form changes to support locations
    - Campsite Model modified to support locations
    - Map on Show page modified, now includes onClick event to show more information.
    - Routes updated to accommodate locations
    
    *** Additional Changes ***
    Temp workaround for Logo being broken during deployment.

--- models/campsite.js
+++ models/campsite.js
@@ -7,6 +7,9 @@ var campsiteSchema = new mongoose.Schema({
     price: String,
     image: String,
     description: String,
+    location: String,
+    lat: Number,
+    lng: Number,
     author: {
         id: {
             type: mongoose.Schema.Types.ObjectId,
