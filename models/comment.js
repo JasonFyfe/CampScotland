796fd9f6a7e9535009d169622049685d752dc831
Jason <jasonfyfe.dev@gmail.com>
2020-05-30 15:29:36 +0100

    Moment.js Added
    Added Moment.js to the project to allow for creation times and dates of Campsites/Comments to be store.
    
    - Changed show.ejs to display creation times of both Campsite and Comments.
    - Updated Campsite and Comment Models to allow creation dates to be stored.
    
    *** Additional Changes ***
    - Added 'required' tags to both comment/new & comment/edit to prevent empty comments being added.

--- models/comment.js
+++ models/comment.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 // SCHEMA SETUP
 var commentSchema = new mongoose.Schema({
     text: String,
+    createdAt: { type: Date, default: Date.now},
     author: {
         id: {
             type: mongoose.Schema.Types.ObjectId,
