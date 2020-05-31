3799509a4068ddb582bb5fdc5f27dbb8564acfdf
Jason <jasonfyfe.dev@gmail.com>
2020-05-31 03:00:13 +0100

    User Updates
    Updated the following for Users:
    - models
    - routes
    - middleware
    - views
    
    Edit Profile still W.I.P
    Revised Campsites index, show pages.

--- models/user.js
+++ models/user.js
@@ -4,11 +4,12 @@ var passportLocalMongoose = require('passport-local-mongoose');
 var UserSchema = new mongoose.Schema({
     username: String,
     password: String,
-    avatar: String,
+    avatar: {type: String, default: "https://banner2.cleanpng.com/20180521/ocp/kisspng-computer-icons-user-profile-avatar-french-people-5b0365e4f1ce65.9760504415269493489905.jpg"},
     firstName: String,
     lastName: String,
     email: String,
-    isAdmin: {type: Boolean, default: false}
+    isAdmin: {type: Boolean, default: false},
+    createdAt: { type: Date, default: Date.now}
 });
 
 UserSchema.plugin(passportLocalMongoose);
